8cf5792446ed01192b6badc4435aa2d3078bbf64
Matěj Minařík <majsek555@gmail.com>
2023-10-04 20:31:08 +0200

    enemy rotates to player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -32,6 +32,12 @@ func _physics_process(delta):
 	
 func followPlayer() -> void:
 	var follow_direction : Vector3 = get_global_position().direction_to(player_.get_global_position())
+	var angle : float = rad_to_deg(Vector3(1, 0, 0).angle_to(Vector3(follow_direction.x, 0, follow_direction.z)))
+	if follow_direction.z > 0 :
+		angle = 360 - angle
+	self.set_rotation_degrees(Vector3(0,angle+90,0))
+	
+	print(follow_direction)
 	follow_direction.y = 0
 	set_velocity(follow_direction)
 	if follow_direction != Vector3(0,0,0):

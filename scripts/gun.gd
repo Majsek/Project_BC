d15337e9e069df52b229b8a60be57f954a3f5dd3
Matěj Minařík <majsek555@gmail.com>
2024-03-28 12:49:59 +0100

    charge gun balance buff

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -73,7 +73,7 @@ func load_bullets():
 	if shooting_:
 		return
 	if hand_.trigger_clicking_:
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.2).timeout
 		hand_.trigger_haptic_pulse("load_bullet", 1.0, 1.0, 0.3, 0.3 )
 		bullets_ += 1
 		print(bullets_)
